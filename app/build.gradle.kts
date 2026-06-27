ec5dc91c642afd98b150087c5da299f7fc408c83
Jmetrics86 <jsnbrwstr@gmail.com>
2026-06-26 21:32:16 -0400

    bump version to 0.0.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.endlessseven.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 19
-        versionName = "0.0.19"
+        versionCode = 20
+        versionName = "0.0.20"
     }
 
     buildTypes {
